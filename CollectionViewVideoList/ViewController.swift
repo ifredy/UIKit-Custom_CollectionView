a68fcfa5963552a51e2b6a8efc954fa9ca39e222
Fredy dev <freddy@pixel.pe>
2024-06-21 10:56:48 -0500

    Initial Commit

---
+++ CollectionViewVideoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewVideoList
+//
+//  Created by Fredy asencios M1 on 21/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
